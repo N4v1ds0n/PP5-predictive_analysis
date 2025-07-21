9b02b1ebce76327164dc9f15b014bd1f8d7d4460
N4v1ds0n <damian.droste@gmx.de>
2025-07-21 16:00:37 +0200

    Add app page for streamlit dashboard and loaded first 3 pages into dashboard

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_visual_diagnosis_assistant import page_visual_diagnosis_assistant_body
 from app_pages.page_summary import page_summary_body
+from app_pages.page_mildew_detector import page_mildew_detector_body
 
 app = MultiPage(app_name="Mildew Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Diagnosis Assistant", page_visual_diagnosis_assistant_body)
 app.add_page("Summary", page_summary_body)
+app.add_page("Mildew Detector", page_mildew_detector_body)
 
 if __name__ == "__main__":
     app.run()
